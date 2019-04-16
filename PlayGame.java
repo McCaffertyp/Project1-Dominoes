dfeb8271d7c4ca5ec43c8fc6ccf32400aae5928e
McCaffertyp <48999663+McCaffertyp@users.noreply.github.com>
2019-04-16 16:18:24 -0700

    Create PlayGame.java

---
+++ PlayGame.java
@@ -0,0 +1,11 @@
+public class PlayGame {
+
+    public static void main(String[] args) {
+        PlayGame play = new PlayGame();
+        play.run();
+    }
+    
+    public static void run() {
+        
+    }
+}
