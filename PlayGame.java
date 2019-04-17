1a33d4629caba013b6458d299f599d9409eedb01
1MagicStar <49054703+1MagicStar@users.noreply.github.com>
2019-04-17 13:23:38 -0700

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -5,7 +5,14 @@ public class PlayGame {
         play.run();
     }
     
-    public static void run() {
+    public static void run() 
+    {
+        //Intro information
+        System.out.println("Welcome to the game of Dominos!");
+        System.out.println("When you are read type start and hit enter")
+        
+        //this string will be used throughout the game to read the users input
+        string intput = "";
         
     }
 }
