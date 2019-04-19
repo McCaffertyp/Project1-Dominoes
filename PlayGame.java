85f2df888c02ae3d222a07541e3432362bc8786b
1MagicStar <49054703+1MagicStar@users.noreply.github.com>
2019-04-19 12:50:06 -0700

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PlayGame {
 
     public static void main(String[] args) {
@@ -9,10 +11,43 @@ public class PlayGame {
     {
         //Intro information
         System.out.println("Welcome to the game of Dominos!");
-        System.out.println("When you are read type start and hit enter")
+        System.out.println("When you are read type start and hit enter");
         
-        //this string will be used throughout the game to read the users input
-        string intput = "";
+        //this will be true when the user is ready to start
+        boolean startGame = false;
         
-    }
-}
+        //loop until ready to start
+        while(startGame == false)
+        {
+
+           //this string will be used throughout the game to read the users input
+           String input = "";
+           
+           Scanner user = new Scanner(System.in);
+           
+           //get the users info
+           input = user.nextLine();
+           
+           //this should see if the user wants to start the game
+           //some of these are for spelling errors.
+           if(input.equals("start") || input.equals("go") || input.equals("begin") || input.equals("yes") || input.equals("sart") || input.equals("stat") || input.equals("star"))
+           {
+               startGame = true;
+           }
+           else if(input.equals("exit") || input.equals("quit"))
+           {
+               return;
+           }
+           else
+           {
+               System.out.println("Please type start to start the game! If you want to quit type exit!");
+           }
+           
+           
+        }//while loop for starting the game
+        
+        
+        
+    }//run
+
+}//Class
