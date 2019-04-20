1c4ada2339021187448f29db54891fe03870e190
1MagicStar <49054703+1MagicStar@users.noreply.github.com>
2019-04-19 17:10:58 -0700

    Update Players.java

--- Players.java
+++ Players.java
@@ -1,6 +1,28 @@
 public class Players
 {
+    private Domino[] hand = new Domino[21];
+    private String name;
+    private int howManyDominos;
     public Players()
     {
+    }
+    public Players(String nametemp, int howManyDominostemp)
+    {
+         name = nametemp;
+         howManyDominos = howManyDominostemp;
+    }
+    
+    //getter methods
+    public String getName()
+    {
+      return name;
+    }
+    public int gethowManyDominos()
+    {
+      return howManyDominos;
+    }
+    public Domino[] gethand()
+    {
+         return hand;
     }
 }
