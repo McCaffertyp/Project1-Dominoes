8dd1c77e1b136550f711e3fdebe320737066afce
1MagicStar <49054703+1MagicStar@users.noreply.github.com>
2019-04-19 17:11:27 -0700

    Update Domino.java

--- Domino.java
+++ Domino.java
@@ -1,5 +1,25 @@
-public class Domino {
-    public static void main(String[] args) {
-        System.out.println("Cheese");
+public class Domino 
+{
+
+    //hold both numbers that the dominos has
+    private int firstNumber;
+    private int secondNumber;
+    Domino()
+    {
+    }
+    Domino(int numberOne, int numberTwo)
+    {
+         firstNumber = numberOne;
+         secondNumber = numberTwo;
+    }
+    
+    //two getter methods
+    public int getDominoNumberOne()
+    {
+         return firstNumber;
+    }
+    public int getDominoNumberTwo()
+    {
+         return secondNumber;
     }
 }
