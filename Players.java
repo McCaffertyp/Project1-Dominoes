0a1b237f7d78d37dd41c618ddd4230fe3462bfb6
McCaffertyp <48999663+McCaffertyp@users.noreply.github.com>
2019-04-20 13:39:02 -0700

    Update Players.java
    Mainly added the assignHands method

--- Players.java
+++ Players.java
@@ -1,28 +1,44 @@
 public class Players
 {
-    private Domino[] hand = new Domino[21];
+    private Domino[] hand;
     private String name;
-    private int howManyDominos;
-    public Players()
-    {
+    private DRandom random = new DRandom(); //To access Random
+    
+    public Players() {
     }
-    public Players(String nametemp, int howManyDominostemp)
-    {
-         name = nametemp;
-         howManyDominos = howManyDominostemp;
+    
+    public Players(String nameTemp, int howManyDominosTemp) {
+         name = nameTemp;
+         hand = new Domino[howManyDominosTemp];
+    }
+    
+    public void assignHands(Players player1, Players player2, Board board) {
+        Domino[] domChange = board.getDomsAvail();
+        int boardLen = domChange.length, track = 0, ranNum;
+        for (int i = 0; i < hand.length*2; i++) {
+            ranNum = random.getRandNum(0, boardLen);
+            if (i % 2 == 0) { //Domino goes to human
+                player1.hand[i-track] = domChange[ranNum];
+            } else if (i % 2 == 1) { //Domino goes to AI
+                player2.hand[i-track] = domChange[ranNum];
+            }
+            for (int j = ranNum; j < boardLen-1; j++) {
+                domChange[j] = domChange[j+1];
+            }
+            domChange = Arrays.copyOfRange(domChange, 0, boardLen-1);
+            boardLen--;
+        }
+        board.setDomsAvail(domChange);
     }
     
     //getter methods
-    public String getName()
-    {
+    public String getName() {
       return name;
     }
-    public int gethowManyDominos()
-    {
-      return howManyDominos;
+    public int gethowManyDominos() {
+      return hand.length;
     }
-    public Domino[] gethand()
-    {
+    public Domino[] gethand() {
          return hand;
     }
 }
