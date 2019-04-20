5665d504e3f106f211bc6076a88b8eb064afd6f6
McCaffertyp <48999663+McCaffertyp@users.noreply.github.com>
2019-04-19 20:11:12 -0700

    Update Random.java

--- Random.java
+++ Random.java
@@ -1,9 +1,18 @@
-public class Random 
-{
-    Random()
-    {
-    }
-    Random(int low, int high)
-    {
-    }
+import java.util.*;
+import java.io.*;
+
+/*
+   Get a random number in a certain range from lowVal to highVal
+   Return value gotten.
+*/
+public class Random {
+   private int randNum;
+   Random r = new Random();
+   
+   public Random() {};
+   
+   private int getRandNum(int lowVal, int highVal) {
+      randNum = r.nextInt(highVal) + lowVal; //Includes lowVal but not highVal
+      return randNum;
+   }
 }
