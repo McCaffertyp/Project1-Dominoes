b0b294a47470b1a36c61cfec2d32664b57ae4781
McCaffertyp <48999663+McCaffertyp@users.noreply.github.com>
2019-04-21 19:58:26 -0700

    Update Domino.java
    Domino class with necessary method addition for domino rotation.

--- Domino.java
+++ Domino.java
@@ -1,24 +1,35 @@
+/*
+    Holds and stores information about each domino.
+*/
 public class Domino 
 {
-
     //hold both numbers that the dominos has
     private int firstNumber;
     private int secondNumber;
+    
     Domino() {
     }
     
-    //Creates a Domino with two numbers; pos one and pos two.
+    //Creates a Domino with two numbers in position one and two
     Domino(int numberOne, int numberTwo) {
          firstNumber = numberOne;
          secondNumber = numberTwo;
     }
     
+    //Number swap method in case the second number matches the first.
+    public Domino swapNums(Domino domTemp) {
+        int swap = domTemp.secondNumber;
+        domTemp.secondNumber = domTemp.firstNumber;
+        domTemp.firstNumber = swap;
+        return domTemp;
+    }
+    
     //two getter methods
-    public int getDominoNumberOne() {
+    public int getDomNumOne() {
          return firstNumber;
     }
     
-    public int getDominoNumberTwo() {
+    public int getDomNumTwo() {
          return secondNumber;
     }
 }
